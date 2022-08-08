c823049c22d49d68d0682c0a66592a248aa453e3
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-08-09 01:08:29 +0200

    remove class based components

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,82 +1,77 @@
-import React, { useState,useEffect } from "react"
-import Header from "./Header"
-import InputTodo from "./InputTodo"
-import TodosList from "./TodoList"
-import { v4 as uuidv4 } from "uuid"
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import TodosList from './TodoList';
 
 const TodoContainer = () => {
-  const [todos, setTodos] = useState(getInitialTodos())
+  const [todos, setTodos] = useState(getInitialTodos());
 
-  const handleChange = id => {
-    setTodos(prevState =>
-      prevState.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
-          }
-        }
-        return todo
-      })
-    )
-  }
+  const handleChange = (id) => {
+    setTodos((prevState) => prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    }));
+  };
 
-  const delTodo = id => {
+  const delTodo = (id) => {
     setTodos([
-      ...todos.filter(todo => {
-        return todo.id !== id
-      }),
-    ])
-  }
+      ...todos.filter((todo) => todo.id !== id),
+    ]);
+  };
 
-  const addTodoItem = title => {
+  const addTodoItem = (title) => {
     const newTodo = {
       id: uuidv4(),
-      title: title,
+      title,
       completed: false,
-    }
-    setTodos([...todos, newTodo])
-  }
+    };
+    setTodos([...todos, newTodo]);
+  };
 
   const setUpdate = (updatedTitle, id) => {
     setTodos(
-      todos.map(todo => {
+      todos.map((todo) => {
         if (todo.id === id) {
-          todo.title = updatedTitle
+          todo.title = updatedTitle;
         }
-        return todo
-      })
-    )
-  }
+        return todo;
+      }),
+    );
+  };
 
   // useEffect(() => {
   //   console.log("test run")
-  
+
   //   // getting stored items
   //   const temp = localStorage.getItem("todos")
   //   const loadedTodos = JSON.parse(temp)
-  
+
   //   if (loadedTodos) {
   //     setTodos(loadedTodos)
   //   }
   // }, [])
   function getInitialTodos() {
     // getting stored items
-    const temp = localStorage.getItem("todos")
-    const savedTodos = JSON.parse(temp)
-    return savedTodos || []
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
   }
 
+  useEffect(() => {
+    console.log('use effect running');
+    // storing todos items
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
 
-useEffect(() => {
-  console.log("use effect running");
-  // storing todos items
-  const temp = JSON.stringify(todos)
-  localStorage.setItem("todos", temp)
-}, [todos])
-
-return (
-     <div className="container">
+  return (
+    <div className="container">
       <div className="inner">
         <Header />
         <InputTodo addTodoProps={addTodoItem} />
@@ -88,8 +83,8 @@ return (
         />
       </div>
     </div>
-   
-)
-}
 
-export default TodoContainer
+  );
+};
+
+export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-//for new react version instead of using ReactDom use createRoot 
-import { createRoot } from "react-dom/client"
-//import component main file
-import TodoContainer from "./functionBased/components/TodoContainer";
-//import css
-import "./functionBased/App.css"
-//import treact router
-import { BrowserRouter as Router } from "react-router-dom";
-const container = document.getElementById("root");
+import React from 'react';
+// for new react version instead of using ReactDom use createRoot
+import { createRoot } from 'react-dom/client';
+// import component main file
+import TodoContainer from './functionBased/components/TodoContainer';
+// import css
+import './functionBased/App.css';
+// import treact router
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Router>
-    <TodoContainer />
+      <TodoContainer />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
